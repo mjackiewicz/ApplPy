ac20b30fb155b88d0199e9a3d75dd314ce7e9435
Matthew Robinson <mthw.wm.robinson@gmail.com>
2012-05-21 22:46:03 -0400

    auto menu load added

--- APPLPY.py
+++ APPLPY.py
@@ -6,7 +6,5 @@ from sympy import *
 x=Symbol('x');y=Symbol('y');z=Symbol('z');t=Symbol('t')
 sys.displayhook=pprint
 from applpy.applpy import *
-print 'WELCOME TO APPLPY'
-print '_________________'
-print ""
+
 Menu()
